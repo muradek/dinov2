f4e616ae2a3164320b047d74db299cf8f20d8c51
Murad Ektilat <muradek@campus.technion.ac.il>
2024-07-15 01:28:10 +0300

    changed file structure for modularity

--- fine_tune.py
+++ fine_tune.py
@@ -19,23 +19,50 @@ class Dinov2Tune(nn.Module):
         super(Dinov2Tune, self).__init__()
         self.backbone_model = deepcopy(backbone_model)
         self.labels_head = nn.Sequential(nn.Linear(1024, 256), nn.ReLU(), nn.Linear(256, out_dim))
+        # is this enough layers? need to make sure that the head is big enough so that the last layers of dino will output features
+        # and do not change to "detection layers"
 
     def forward(self, frame):
         features = self.backbone_model(frame)
-        print("features size is: ", features.size())
+        # print("features size is: ", features.size())
         labels_prob = self.labels_head(features)
-        print("labels size is: ", labels_prob.size())
-        with torch.no_grad(): # this is a non differentiable operation
+        # print("labels size is: ", labels_prob.size())
+        # with torch.no_grad(): # this is a non differentiable operation
             # one_hot_vector = F.one_hot(torch.argmax(labels_prob), num_classes=labels_prob.size(0)).float()
-            max_index = torch.argmax(labels_prob)
-            output = torch.zeros_like(labels_prob)
-            output[0, max_index] = 1
+        max_index = torch.argmax(labels_prob)
+        output = torch.zeros_like(labels_prob)
+        output[0, max_index] = 1
 
-        print("output size is: ", output.size())
+        # print("output size is: ", output.size())
         return output
 
+def create_model():
+    REPO_PATH = "/home/muradek/project/DINO_dir/dinov2" # Specify a local path to the repository (or use installed package instead)
+    sys.path.append(REPO_PATH)
+    
+    BACKBONE_SIZE = "large" # in ("small", "base", "large" or "giant")
 
-if __name__ == "__main__":
+    backbone_archs = {
+        "small": "vits14",
+        "base": "vitb14",
+        "large": "vitl14",
+        "giant": "vitg14",
+    }
+    backbone_arch = backbone_archs[BACKBONE_SIZE]
+    backbone_name = f"dinov2_{backbone_arch}"
+
+    backbone_model = torch.hub.load(repo_or_dir="facebookresearch/dinov2", model=backbone_name)
+    backbone_model.eval()
+    # backbone_model.cuda()
+
+    out_dim = 11 # number of classes for detection
+    model = Dinov2Tune(backbone_model, out_dim) # maybe pass reference of the model, or model args and consruct it in the __init__?
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = model.to(device) 
+    
+    return model
+
+def main():
     REPO_PATH = "/home/muradek/project/DINO_dir/dinov2" # Specify a local path to the repository (or use installed package instead)
     sys.path.append(REPO_PATH)
     
@@ -62,6 +89,8 @@ if __name__ == "__main__":
     # load frame for inference
     frame_path = "/home/muradek/project/DINO_dir/dino/frames/frame-0012.jpg"
     frame = Image.open(frame_path) 
+
+    # Todo: this should be changed to a transform module!!!
     new_height = (frame.height // 14) * 14  # Round down to the nearest multiple of 14
     new_width = (frame.width // 14) * 14
     resized_image = frame.resize((new_width, new_height))
@@ -80,3 +109,6 @@ if __name__ == "__main__":
     with torch.inference_mode():
         output = model(final_img) 
     print(output)
+
+if __name__ == "__main__":
+    main()
